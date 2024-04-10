a445a3aa68da2358766ff9a2029ed4058bef9665
ErinKace <erinkacerovskis@gmail.com>
2024-04-09 21:45:23 -0500

    reintitalized through spring

--- src/main/java/com/npclibrary/NpcLibraryApplication.java
+++ src/main/java/com/npclibrary/NpcLibraryApplication.java
@@ -1,4 +1,4 @@
-package com.npclibrary.demo;
+package com.npclibrary;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/npclibrary/NpcLibraryApplicationTests.java
+++ src/test/java/com/npclibrary/NpcLibraryApplicationTests.java
@@ -1,4 +1,4 @@
-package com.npclibrary.demo;
+package com.npclibrary;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
