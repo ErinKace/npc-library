45b8d6e072a2acfd339b9db467c9d64a2aae04f5
ErinKace <erinkacerovskis@gmail.com>
2024-04-09 21:40:12 -0500

    first commit

---
+++ src/main/java/com/npclibrary/demo/NpcLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.npclibrary.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NpcLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NpcLibraryApplication.class, args);
+	}
+
+}
